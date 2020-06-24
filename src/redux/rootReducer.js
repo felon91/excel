c032aa054f6aeedbe04b7eecf826214ee5b5d8e8
Denis <felon_91@mail.ru>
2020-06-24 14:21:54 +0300

    finish routing

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,4 +1,4 @@
-import {CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLE, CHANGE_TITLE} from '@/redux/types';
+import {CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLE, CHANGE_TITLE, UPDATE_DATE} from '@/redux/types';
 
 export function rootReducer(state, action) {
   let field;
@@ -30,6 +30,9 @@ export function rootReducer(state, action) {
     }
     case CHANGE_TITLE: {
       return {...state, title: action.data};
+    }
+    case UPDATE_DATE: {
+      return {...state, openedDate: new Date().toJSON()};
     }
     default: return state;
   }
